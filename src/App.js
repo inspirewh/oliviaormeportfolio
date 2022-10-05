884117e47757cd12cec0d3275aea44a4a2a3fc7c
inspirewh <78182677+inspirewh@users.noreply.github.com>
2022-10-05 14:47:07 +0800

    route update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //import logo from './logo.svg';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home';
@@ -7,24 +7,40 @@ import About from './pages/About';
 import Resume from './pages/Resume';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
-import { NavBar } from "./components/NavBar";
-import { Footer } from "./components/Footer";
  
 function App() {
-  return(
-    <>
-    <Router>
-      <Routes>
-        <Route path="/home" exact element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/resume" element={<Resume />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/contact" element={<Contact />} />
-      </Routes>
-      </Router>
-    </>
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home/>,
+    },
+    {
+      path: "/about",
+      element: <About/>
+    },
+    {
+      path: "/resume",
+      element: <Resume/>
+    },
+    {
+      path: "/portfolio",
+      element: <Portfolio/>
+    },
+    {
+      path: "/contact",
+      element: <Contact/>
+    },
+  ]);
+
+  return (
+    <div>
+      
+      <RouterProvider router={router}/>
+
+
+
+    </div>
   );
 }
-
 
 export default App;
